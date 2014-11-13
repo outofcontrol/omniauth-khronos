57c364321d59af78f9e120a11fa1177a96b1401f
James Riordon <jriordon@outofcontrol.ca>
2014-11-13 16:42:07 -0500

    Test alternate require

--- lib/omniauth/strategies/khronos.rb
+++ lib/omniauth/strategies/khronos.rb
@@ -1,14 +1,32 @@
-require 'omniauth-oauth2'
+require 'omniauth/strategies/oauth2'
 
 module OmniAuth
   module Strategies
     class Khronos < OmniAuth::Strategies::OAuth2
-      # Give your strategy a name.
+
+      DEFAULT_SCOPE = 'email'
+
       option :name, "Khronos"
 
-      option :client_options, {:site => "https://authserver.khronos.org", :authorize_url => "https://authserver.khronos.org/oauth/authorize"}
+      option :client_options, {
+        :site => 'https://authserver.khronos.org',
+        :authorize_url => "https://authserver.khronos.org/oauth/authorize",
+        :token_url => '/oauth/access_token'
+      }
 
       uid{ raw_info['id'] }
+
+      def authorize_params
+        super.tap do |params|
+          %w[display scope auth_type].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+
+          params[:scope] ||= DEFAULT_SCOPE
+        end
+      end
 
       info do
         {
