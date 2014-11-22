ef73d6224770833e19b4865c95ccb0fa6fb2a3de
James Riordon <jriordon@outofcontrol.ca>
2014-11-21 20:41:00 -0500

    Test new access_token code

--- lib/omniauth/strategies/khronos.rb
+++ lib/omniauth/strategies/khronos.rb
@@ -10,17 +10,15 @@ module OmniAuth
 
       option :client_options, {
         :site => 'https://authserve.khronos.org',
-        :authorize_url => "/oauth/authorize",
         :token_url => '/oauth/access_token'
       }
 
-      uid{ raw_info['id'] }
+      option :access_token_options, {
+        :header_format => 'OAuth %s',
+        :param_name => 'access_token'
+      }
 
-      def authorize_params
-        super.tap do |params|
-          params[:scope] ||= DEFAULT_SCOPE
-        end
-      end
+      uid{ raw_info['email'] }
 
       info do
         {
@@ -38,6 +36,31 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/me').parsed
       end
+
+      def access_token_options
+        options.access_token_options.inject({}) { |h,(k,v)| h[k.to_sym] = v; h }
+      end
+
+      def authorize_params
+        super.tap do |params|
+          %w[display scope auth_type].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+
+          params[:scope] ||= DEFAULT_SCOPE
+        end
+      end
+
+      protected
+
+      def build_access_token
+        super.tap do |token|
+          token.options.merge!(access_token_options)
+        end
+      end
+
     end
   end
 end
