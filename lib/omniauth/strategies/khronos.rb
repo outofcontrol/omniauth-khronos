c8f1c400be946f04c3402dac080bd6a3e2006483
James Riordon <jriordon@outofcontrol.ca>
2014-11-24 10:13:07 -0500

    Final working copy

--- lib/omniauth/strategies/khronos.rb
+++ lib/omniauth/strategies/khronos.rb
@@ -1,64 +1,45 @@
-require 'omniauth/strategies/oauth2'
+require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
     class Khronos < OmniAuth::Strategies::OAuth2
 
-      DEFAULT_SCOPE = 'email'
-
-      option :name, "Khronos"
-
       option :client_options, {
         :site => 'https://authserve.khronos.org',
+        :authorize_url => 'https://authserve.khronos.org/oauth/authorize',
         :token_url => '/oauth/access_token'
       }
 
-      option :access_token_options, {
-        :header_format => 'OAuth %s',
-        :param_name => 'access_token'
-      }
-
-      uid{ raw_info['email'] }
-
-      info do
-        {
-          :username => raw_info['username'],
-          :email => raw_info['email']
-        }
-      end
-
-      extra do
-        {
-          'raw_info' => raw_info
-        }
-      end
-
-      def raw_info
-        @raw_info ||= access_token.get('/me').parsed
-      end
-
-      def access_token_options
-        options.access_token_options.inject({}) { |h,(k,v)| h[k.to_sym] = v; h }
+      def request_phase
+        super
       end
 
       def authorize_params
         super.tap do |params|
-          %w[display scope auth_type].each do |v|
+          %w[scope client_options].each do |v|
             if request.params[v]
               params[v.to_sym] = request.params[v]
             end
           end
-
-          params[:scope] ||= DEFAULT_SCOPE
         end
       end
 
-      protected
+      uid { raw_info['id'].to_s }
 
-      def build_access_token
-        super.tap do |token|
-          token.options.merge!(access_token_options)
-        end
+      info do
+        {
+          :email => raw_info['email'],
+          :name => raw_info['name'],
+          :nickname => raw_info['username']
+        }
+      end
+
+      extra do
+        {:raw_info => raw_info}
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/v1/users?access_token=#{access_token.token}").parsed
       end
 
     end
